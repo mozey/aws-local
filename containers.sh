da94ebf0fb7e8edf3854f8d72d12b3dd19e7a087
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2018-07-27 13:09:30 +0200

    containers

--- containers.sh
+++ containers.sh
@@ -3,17 +3,20 @@
 # Create containers
 
 # DynamoDB
+docker rm dynamodb
 docker create --name=dynamodb -p 8000:8000 \
 -v ${HOME}/.aws-local/dynamodb/data:/data \
 dwmkerr/dynamodb -dbPath /data -sharedDb
 
 # ES
 # Port 9300 not used, but keep it free in case
+docker rm elastic
 docker create --name=elastic -p 9200:9200 \
 -v ${HOME}/.aws-local/elastic/data:/usr/share/elasticsearch/data \
 elasticsearch:5.6.10
 
 # RDS: MySQL
+docker rm mysql
 docker create --name=mysql -p 3306:3306 \
 -e MYSQL_ROOT_PASSWORD=asdf \
 -e MYSQL_ROOT_HOST=% \
@@ -23,6 +26,7 @@ mysql/mysql-server:5.6
 
 # S3: minio
 # Port 9000 is used for PHP xdebug
+docker rm minio
 docker create --name=minio -p 9002:9000 \
 -e MINIO_ACCESS_KEY=asdf \
 -e MINIO_SECRET_KEY=asdfasdf \
@@ -30,10 +34,12 @@ docker create --name=minio -p 9002:9000 \
 minio/minio server /data
 
 # SES: aws-ses-local
+docker rm aws-ses-local
 docker create --name=aws-ses-local -p 9001:9001 \
 -v ${HOME}/.aws-local/aws-ses-local/data:/aws-ses-local/output \
 jdelibas/aws-ses-local
 
 # SQS & SNS: goaws
+docker rm goaws
 docker create --name goaws -p 4100:4100 pafortin/goaws
 
